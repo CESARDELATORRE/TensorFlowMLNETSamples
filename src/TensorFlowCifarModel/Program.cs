80930eeb5d84f4a3750d6a6fc61cbc2f31704a15
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-09-03 14:56:24 -0700

    Initial ML.NET TensorFlowCifarModel code

---
+++ src/TensorFlowCifarModel/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.ImageAnalytics;
+using Microsoft.ML.Runtime.LightGBM;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TensorFlowCifarModelScoring
+{
+    class Program
+    {
+        static FileInfo currentAssemblyLocation = new FileInfo(typeof(Program).Assembly.Location);
+        static private readonly string _rootDir = currentAssemblyLocation.Directory.FullName;
+        static private readonly string _dataRoot = Path.Combine(_rootDir, "data");
+
+        static void Main(string[] args)
+        {
+            var imageHeight = 32;
+            var imageWidth = 32;
+            var model_location = "cifar_model/frozen_model.pb";
+            var dataFile = GetDataPath("tags/images.tsv");
+            var tagsFolder = Path.GetDirectoryName(dataFile);
+            var imagesFolder = Path.Combine(_dataRoot, "images");
+
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new Microsoft.ML.Data.TextLoader(dataFile).CreateFrom<CifarData>(useHeader: false));
+            pipeline.Add(new ImageLoader(("ImagePath", "ImageReal"))
+            {
+                ImageFolder = imagesFolder
+            });
+
+            pipeline.Add(new ImageResizer(("ImageReal", "ImageCropped"))
+            {
+                ImageHeight = imageHeight,
+                ImageWidth = imageWidth,
+                Resizing = ImageResizerTransformResizingKind.IsoCrop
+            });
+
+            pipeline.Add(new ImagePixelExtractor(("ImageCropped", "Input"))
+            {
+                UseAlpha = false,
+                InterleaveArgb = true
+            });
+
+            pipeline.Add(new TensorFlowScorer()
+            {
+                ModelFile = model_location,
+                InputColumns = new[] { "Input" },
+                OutputColumn = "Output"
+            });
+
+            pipeline.Add(new ColumnConcatenator(outputColumn: "Features", inputColumns: "Output"));
+            pipeline.Add(new TextToKeyConverter("Label"));
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            var model = pipeline.Train<CifarData, CifarPrediction>();
+            string[] scoreLabels;
+            model.TryGetScoreLabelNames(out scoreLabels);
+
+            //Test Scoring
+
+            CifarPrediction prediction = model.Predict(new CifarData()
+            {
+                ImagePath = GetDataPath("images/banana.jpg")
+            });
+
+            Console.WriteLine("End of process");
+        }
+
+        static protected string GetDataPath(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return null;
+            return Path.GetFullPath(Path.Combine(_dataRoot, name));
+        }
+    }
+
+    public class CifarData
+    {
+        [Column("0")]
+        public string ImagePath;
+
+        [Column("1")]
+        public string Label;
+    }
+
+    public class CifarPrediction
+    {
+        [ColumnName("Score")]
+        public float[] PredictedLabels;
+    }
+}
