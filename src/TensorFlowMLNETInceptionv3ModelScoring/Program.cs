5e8bea2062b529c85bd8db5907162096367a709f
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-09-04 12:44:01 -0700

    Added brand new TensorFlowMLNETInceptionv3Scoring project

---
+++ src/TensorFlowMLNETInceptionv3ModelScoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TensorFlowMLNETInceptionv3ModelScoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
